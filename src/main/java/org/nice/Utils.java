7ebc43486ec1a004645f7472ecaf787bbbdebc5e
Richard <richard232manansala@gmail.com>
2024-04-14 19:51:47 +0800

    Fixed mistake in Utils.getResource, Decoding the given resourcePath instead of the returned path

--- src/main/java/org/nice/Utils.java
+++ src/main/java/org/nice/Utils.java
@@ -1,16 +1,16 @@
 package org.nice;
 
-import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 public class Utils {
-    public static URL getResource(String resourcePath) {
+    public static String getResource(String resourcePath) {
         try {
-            return Objects.requireNonNull(
-                    Utils.class.getResource(URLDecoder.decode(resourcePath, StandardCharsets.UTF_8))
+            var path = Objects.requireNonNull(
+                    Utils.class.getResource(resourcePath)
             );
+            return URLDecoder.decode(path.getPath(), StandardCharsets.UTF_8);
         }catch(Exception e) {
             throw  new RuntimeException("The file with path: " + resourcePath + " does not exists.");
         }
